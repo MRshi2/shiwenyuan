0c861ebdbb43664d6ae99ca7e2ababf4e0a4b186
MRshi2 <2574016161@qq.com>
2017-10-31 23:11:21 +0800

    ''

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="div1">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.div1{
+		background: #C9302C;
+	}
+</style>
